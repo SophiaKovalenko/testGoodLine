eb0283f6cf29a1c708a8d21e2ad27ec7d63f2a02
SophiaKovalenko <68947036+SophiaKovalenko@users.noreply.github.com>
2020-11-16 21:18:04 +0700

    Update autoTestYandex2.py

--- autoTestYandex2/autoTestYandex2.py
+++ autoTestYandex2/autoTestYandex2.py
@@ -1,34 +1,54 @@
+'''
+Для запуска программы требуется установить Python 3.5 или выше
+Установить Selenium: pip install selenium
+Скачать и скопировать в папку к Chrome драйвер https://chromedriver.chromium.org/downloads
+'''
+
 from selenium import webdriver
-import time
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome('C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe')
-driver.get('https://rasp.yandex.ru/')
+def goToSite(driver,site):
+    driver.get(site)
 
-from_elem = driver.find_element_by_id("from")
-from_elem.clear()
-from_elem.send_keys("Кемерово проспект Ленина")
+def browser(path):
+    driver = webdriver.Chrome(path)
+    return driver
 
-to_elem = driver.find_element_by_id("to")
-to_elem.clear()
-to_elem.send_keys("Кемерово Бакинский переулок")
+def findElementIdAndSendKeys(driver, name_elem, send_text):
+    elem = driver.find_element_by_id(name_elem)
+    elem.clear()
+    elem.send_keys(send_text)
+    return elem
 
-when_elem = driver.find_element_by_id("when")
-when_elem.clear()
-when_elem.send_keys("среда")
+if __name__ == '__main__':
+    path_driver = "C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe" #Заменить путь до драйвера
+    driver = browser(path_driver)
+    goToSite(driver, 'https://rasp.yandex.ru/')
 
-button_list = driver.find_elements_by_class_name('RadioButton__buttonLable')
+    button_list = driver.find_elements_by_class_name('RadioButton__buttonLable')
+    for button in button_list:
+        if button.text == 'Автобус':
+            button_bus = button
+    button_bus.click()
 
-for button in button_list:
-    if button.text == 'Автобус':
-        button_bus = button
-button_bus.click()
+    findElementIdAndSendKeys(driver, "from", "Кемерово проспект Ленина")
+    findElementIdAndSendKeys(driver, "to", "Кемерово Бакинский переулок")
+    findElementIdAndSendKeys(driver, "when", "среда").submit()
 
-when_elem.submit()
-time.sleep(5)
+    error_elem = None
 
-error_elem = None
-error_elem = driver.find_elements_by_class_name('PopupError')
-for err in error_elem:
-    if err.text == 'Пункт отправления не найден':
-        print("YES:",err.text)
-        break
+    try:
+        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "PopupError")))
+    except TimeoutError:
+        print("A TimeoutError!")
+    else:
+        error_elem = driver.find_elements_by_class_name('PopupError')
+        for err in error_elem:
+            if err.text == 'Пункт отправления не найден':
+                print("Тест пройден:", err.text)
+                break
+    finally:
+        driver.quit()
+        print("THE END")
