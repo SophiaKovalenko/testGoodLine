e585cb43f6d02234e774613d407d4262e1fbb209
SophiaKovalenko <68947036+SophiaKovalenko@users.noreply.github.com>
2020-11-16 21:33:29 +0700

    Update main.py

--- testAPIYandex/main.py
+++ testAPIYandex/main.py
@@ -1,37 +1,32 @@
+'''
+Для запуска программы требуется установить Python 3.5 или выше
+Установить requests: pip install requests
+*Используется ключ: {6e715e39-9e14-4981-9e3c-a4a3d5b5251c}
+'''
+
 import requests
 
 def nearest_stationsGet(payload_nearest_stations):
     # Список ближайших станций
     print('------------------------------')
-    print('def nearest_stationsGet():')
+    print('def nearest_stationsGet(): Список ближайших станций')
 
     base_URL = 'https://api.rasp.yandex.net/v3.0/nearest_stations/?'
     r = requests.get(base_URL, params=payload_nearest_stations)
 
     print(f"url: {r.url}")
-    #print(f"text: \n {r.text}")
-
-    r_dict=r.json()
+    r_dict = r.json()
     number_station = int(r_dict['pagination']['total'])
     print(f"В радиусе {payload_nearest_stations['distance']}км от Б15 кол-во станций:{number_station}")
 
     for i in range(number_station):
 
         transport_type = r_dict['stations'][i]['transport_type']
-        if transport_type == 'plane':
-            transport_type = 'Самолет'
-        elif transport_type == 'train':
-            transport_type = 'Поезд'
-        elif transport_type == 'bus':
-            transport_type = 'Автобус'
-        else:
-            continue
-
         station_type_name = r_dict['stations'][i]['station_type_name']
         title = r_dict['stations'][i]['title']
         distance = round(r_dict['stations'][i]['distance'],4)
 
-        print(f'{i+1}. Тип транспорта: {transport_type},\t'
+        print(f'{i+1}. Тип транспорта: {transport_type}, '
               f'название: {station_type_name},\t'
               f'название станции: "{title}",\t'
               f'находится на расстоянии {distance}км от Б15')
@@ -39,30 +34,29 @@ def nearest_stationsGet(payload_nearest_stations):
 def nearest_nearest_settlementGet(payload_nearest_settlement):
     # Ближайший город к указанной точке
     print('------------------------------')
-    print('def nearest_nearest_settlementGet(payload_nearest_settlement):')
+    print('def nearest_nearest_settlementGet(payload_nearest_settlement): Ближайший город к указанной точке')
 
     base_URL = 'https://api.rasp.yandex.net/v3.0/nearest_settlement/?'
     r = requests.get(base_URL, params=payload_nearest_stations)
 
     print(f"url: {r.url}")
-    # print(f"text: \n {r.text}")
 
     r_dict = r.json()
     name_settlement = r_dict['title']
     distance = r_dict['distance']
 
-    print(f"В радиусе {payload_nearest_settlement['distance']}км (от Б15) Ближайший город:{name_settlement}, на расстоянии: {round(distance,4)}км")
+    print(f"В радиусе {payload_nearest_settlement['distance']}км (от Б15) Ближайший город:{name_settlement}, его центр на расстоянии: {round(distance,4)}км")
+
 
 def searchGet(payload_search):
     # Расписание рейсов между станциями
     print('------------------------------')
-    print('def searchGet(payload_search):')
+    print('def searchGet(payload_search): Расписание рейсов между станциями')
 
     base_URL = 'https://api.rasp.yandex.net/v3.0/search/?'
 
     r = requests.get(base_URL, params=payload_search)
     print(f"url: {r.url}")
-    # print(f"text: \n {r.text}")
 
     r_dict = r.json()
 
@@ -75,25 +69,13 @@ def searchGet(payload_search):
 
         date_departure = r_dict['segments'][i]['departure'][:10]
         time_departure = r_dict['segments'][i]['departure'][11:19]
-
         date_arrival = r_dict['segments'][i]['arrival'][:10]
         time_arrival = r_dict['segments'][i]['arrival'][11:19]
-
         transport_type = r_dict['segments'][i]['thread']['transport_type']
-        if transport_type == 'plane':
-            transport_type = 'Самолет'
-        elif transport_type == 'train':
-            transport_type = 'Поезд'
-        elif transport_type == 'bus':
-            transport_type = 'Автобус'
-        else:
-            continue
 
         print(f'{i+1}. {transport_type}\t ОТПРАВЛЕНИЕ: дата: {date_departure}, время: {time_departure}; '
               f'ПРИБЫТИЕ: дата {date_arrival}, время: {time_arrival}')
 
-    uid = r_dict['segments'][0]['thread']['uid']
-    return(uid)
 
 if __name__ == '__main__':
 
