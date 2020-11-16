322451dd40819018a810a2d3fecf8255b132c67c
SophiaKovalenko <68947036+SophiaKovalenko@users.noreply.github.com>
2020-11-16 19:01:49 +0700

    Update autoTestYandex.py

--- AutoTestYandex/autoTestYandex.py
+++ AutoTestYandex/autoTestYandex.py
@@ -1,42 +1,62 @@
+'''
+pip install selenium
+pip install pytest
+pip install requests
+Скачать и скопировать в папку к Chrome драйвер https://chromedriver.chromium.org/downloads
+'''
+
+import pytest
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 import time
 
-driver = webdriver.Chrome('C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe')
-driver.get('https://rasp.yandex.ru/')
+def goToSite(driver,site):
+    driver.get(site)
 
-from_elem = driver.find_element_by_id("from")
-from_elem.clear()
-from_elem.send_keys("Кемерово")
+def browser():
+    print("-def browser():")
+    driver = webdriver.Chrome(executable_path="C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe")
+    #yield driver
+    #driver.quit()
+    return driver
 
-to_elem = driver.find_element_by_id("to")
-to_elem.clear()
-to_elem.send_keys("Москва")
+def findElementIdAndSendKeys(driver, name_elem, send_text):
+    print("-def findElementIdAndSendKeys(driver, name_elem, send_text):")
+    elem = driver.find_element_by_id(name_elem)
+    elem.clear()
+    elem.send_keys(send_text)
+    return elem
 
-when_elem = driver.find_element_by_id("when")
-when_elem.clear()
-when_elem.send_keys("7 июля 2018")
-when_elem.submit()
-print('beforeSleep')
-time.sleep(5)
+if __name__ == '__main__':
+    driver = browser()
+    goToSite(driver, 'https://rasp.yandex.ru/')
 
-q_len = len(driver.find_elements_by_class_name('SearchTransfer__toggle'))  # выпадающий список маршрута
+    findElementIdAndSendKeys(driver, "from", "Кемерово")
+    findElementIdAndSendKeys(driver, "to", "Москва")
+    findElementIdAndSendKeys(driver, "when", "7 июля 2018").submit()
+    
+    time.sleep(5)
+    #driver.implicitly_wait(10)
+    #wait = WebDriverWait(driver, 10)
+    #e = wait.until(EC.invisibility_of_element((By.CLASS_NAME,  'SegmentHeader')))
+    #print(e)
 
+    name = driver.find_elements_by_class_name('SegmentTitle__header')
+    print("name", name)
+    title = driver.find_elements_by_class_name('SegmentTitle__title')
+    #print("title", title)
+    number = driver.find_elements_by_class_name('SegmentTitle__number')
+    #print("number", number)
+    time = driver.find_elements_by_class_name('SearchSegment__duration')
+    #print('time', time)
+    for i in range(len(title)):
+        sub_ico = None
+        sub_ico = name[i].find_element_by_class_name('Icon.Icon_flexShrinkZero.TransportIcon__icon') #SegmentTitle__title
+        if sub_ico:
+            print(f"Рейс №{i+1}: {number[i].text} {title[i].text}; Продолжительность пути: {time[i].text}; Иконка есть!")
+        else:
+            print(f"Рейс №{i+1}: {number[i].text} {title[i].text}; Продолжительность пути: {time[i].text}; Иконки нет!")
 
-title_raise = driver.find_elements_by_class_name('SegmentTitle__title')
-print("Кол-во рейсов: ", len(title_raise)-q_len) #Рейсев всего 3 штуки
-
-time_elem = driver.find_elements_by_class_name('SearchSegment__duration')
-print("Время в пути: ",time_elem) #Рейсев всего 3 штуки
-
-icon_elem = driver.find_elements_by_class_name('TransportIcon__icon')
-print("Кол-во иконок транспорта: ",len(icon_elem)) #Рейсев всего 3 штуки
-
-test_title = driver.find_elements_by_class_name('SegmentTitle')
-print(test_title)
-for i in range(len(test_title)):
-    sub_Title_element = None
-    sub_Title_element = test_title[i].find_elements_by_tag_name('figure')
-    if sub_Title_element: print("YES:",sub_Title_element)
-
-#button_search = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/header/div[1]/div/div[5]/form/button[2]')
-#button_search.send_keys(Keys.RETURN)
+    print(f"Всего рейсов на странице:{len(name)} из 5ти")
